f8aaecce7b65761ea36408df9f64df1474883355
Arkadipta-Kundu <arkadiptak49@gmail.com>
2024-10-31 16:20:08 +0530

    Update Hero component to use Google Drive image URL for profile picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,8 @@ export function Hero() {
       <div className="max-w-3xl mx-auto">
         <div className="mb-8">
           <img
-            src="https://imgur.com/a/zLF6qop"
-            alt="Arkadipta Kundu3"
+            src="https://drive.google.com/file/d/18NEfrRt3FGCTg1npec3i-hYq4bcNswCc/view?usp=sharing"
+            alt="Arkadipta Kundu"
             className="w-32 h-32 rounded-full mx-auto object-cover ring-4 ring-blue-500/30"
           />
         </div>
